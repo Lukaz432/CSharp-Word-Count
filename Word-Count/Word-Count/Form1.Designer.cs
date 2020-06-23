529e69afa2df5e33d3addbf5d1e6038b81272a01
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-23 17:27:15 +0300

    Word-Count

---
+++ Word-Count/Word-Count/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Word_Count
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countBtn = new System.Windows.Forms.Button();
+            this.doneBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wordBox = new System.Windows.Forms.TextBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // countBtn
+            // 
+            this.countBtn.Location = new System.Drawing.Point(119, 134);
+            this.countBtn.Name = "countBtn";
+            this.countBtn.Size = new System.Drawing.Size(107, 23);
+            this.countBtn.TabIndex = 0;
+            this.countBtn.Text = "Count Words";
+            this.countBtn.UseVisualStyleBackColor = true;
+            this.countBtn.Click += new System.EventHandler(this.countBtn_Click);
+            // 
+            // doneBtn
+            // 
+            this.doneBtn.Location = new System.Drawing.Point(359, 134);
+            this.doneBtn.Name = "doneBtn";
+            this.doneBtn.Size = new System.Drawing.Size(75, 23);
+            this.doneBtn.TabIndex = 1;
+            this.doneBtn.Text = "Done";
+            this.doneBtn.UseVisualStyleBackColor = true;
+            this.doneBtn.Click += new System.EventHandler(this.doneBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input text below to count words:";
+            // 
+            // wordBox
+            // 
+            this.wordBox.Location = new System.Drawing.Point(31, 62);
+            this.wordBox.MaxLength = 100;
+            this.wordBox.Name = "wordBox";
+            this.wordBox.Size = new System.Drawing.Size(528, 20);
+            this.wordBox.TabIndex = 3;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(253, 134);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear Text";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 196);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.wordBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.doneBtn);
+            this.Controls.Add(this.countBtn);
+            this.Name = "Form1";
+            this.Text = "Word Counter TX-9000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button countBtn;
+        private System.Windows.Forms.Button doneBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox wordBox;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ Word-Count/Word-Count/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Word_Count
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void countBtn_Click(object sender, EventArgs e)
+        {
+            string text = wordBox.Text;
+            char[] separator = { ' ' };
+            int wordsCount = text.Split(separator, StringSplitOptions.RemoveEmptyEntries).Length;
+            MessageBox.Show(wordsCount.ToString());
+        }
+
+        private void doneBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            wordBox.Text = "";
+        }
+    }
+}
